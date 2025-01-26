6e42167def40712d88472477a3df1efb2ede9b8e
Owen Corrigan <owencorrigan76@gmail.com>
2025-01-26 18:05:41 +0000

    First Commit
    Signed-off-by: Owen Corrigan <owencorrigan76@gmail.com>

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -2,17 +2,24 @@ package com.linkedin.collections;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class Application {
 
 	public static void main(String[] args) {
-		
-		Collection c = new ArrayList(); //Same Collection Type, Different Element Type
-		c.add("Cambridge");
-		
-		Collection c2 = new ArrayList(); //Same Collection Type, Different Element Type
-		c2.add(new Room("Cambridge", "Suite", 6, 250.0));
-		
+		List<Guitar> g = new ArrayList<>();
+
+		g.add(new Guitar("Fender", "Strat", 6));
+		g.add(new Guitar("Gibson", "Les Paul", 6));
+
+		g.forEach(e -> System.out.println("Make: " + e.getMake()));
+		System.out.println(g);
+
+		Collection<Room> c = new ArrayList<>(); //Same Collection Type, Different Element Type
+		//Collection c2 = new ArrayList(); //Same Collection Type, Different Element Type
+		c.add(new Room("Cambridge", "Suite", 6, 250.0));
+		c.forEach(e -> System.out.println("Cap: " + e.getCapacity() + "\nName: " + e.getName()));
+
 	}
 
 }
