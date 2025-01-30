3ec296d8a0f130c8272215eeb26512cff5a5b95b
ocorriga <ocorriga@redhat.com>
2025-01-30 15:33:39 +0000

    Commit
    Signed-off-by: ocorriga <ocorriga@redhat.com>

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -13,20 +13,28 @@ public class Application {
 		Room manchester = new Room("Manchester", "Suite", 5, 250.00);
 		Room oxford = new Room("Oxford", "Suite", 5, 225.0);
 		Room victoria = new Room("Victoria", "Suite", 5, 225.00);
-		
+
 		Collection<Room> rooms = new ArrayList<>(Arrays.asList(cambridge, oxford, victoria, manchester));
 		oxford.setPetFriendly(true);
 		victoria.setPetFriendly(true);
-		
+
+        // can't remove from loop. hence add to an arraylist and remove the arraylist after the loop
+        Collection<Room> removeRoom = new ArrayList<>();
 		for(Room room : rooms) {
-			
 			if(room.isPetFriendly()) {
-				rooms.remove(room);
+				removeRoom.add(room);
 			}
-			
 		}
-		
-		System.out.println(rooms);
-		
-	}
+        rooms.removeAll(removeRoom);
+        rooms.forEach(r -> System.out.println(r.getName()));
+
+        // this is the same as above - you can remove from loop using iterator
+        Iterator<Room> iterator = rooms.iterator();
+        for(Room room : rooms) {
+            if(room.isPetFriendly()) {
+                iterator.remove();
+            }
+        }
+        rooms.forEach(r -> System.out.println(r.getName()));
+    }
 }
