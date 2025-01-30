4c1941e5b0b829aeb797546cf4693fb7bd3a0340
ocorriga <ocorriga@redhat.com>
2025-01-30 15:13:21 +0000

    Commit
    Signed-off-by: ocorriga <ocorriga@redhat.com>

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -3,6 +3,7 @@ package com.linkedin.collections;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Iterator;
 
 public class Application {
 
@@ -13,7 +14,17 @@ public class Application {
 		Room westminister = new Room("Westminister", "Premiere Room", 4, 200.00);
 		
 		Collection<Room> rooms = new ArrayList<>(Arrays.asList(piccadilly, cambridge, westminister));
-		
-			
+		Iterator<Room> iterator = rooms.iterator();
+
+		while (iterator.hasNext()){
+			Room room = iterator.next();
+			System.out.println(room.getName());
+		}
+
+		// this is the same as above
+		for(Room room : rooms){
+			System.out.println(room.getName());
+		}
+
 	}
 }
